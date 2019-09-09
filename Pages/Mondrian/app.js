4431020cab04371b5bd39c45108865c8fabd44ea
Molly Graham <mollyclaire.graham@gmail.com>
2019-09-09 16:36:11 -0500

    onclick plays sound, but I want to make hover work.

--- Pages/Mondrian/app.js
+++ Pages/Mondrian/app.js
@@ -1,9 +1,52 @@
-function playSound(event) {
-    const audio = document.querySelector(`audio[id="${event.}"]`);
-    const key = document.querySelector(`div[data-key="${event.keyCode}"]`);
-    if (!audio) return;
+// function playSound(event) {
+//     const audio = document.querySelector(`audio[id="${event.}"]`);
+//     const key = document.querySelector(`div[data-key="${event.keyCode}"]`);
+//     if (!audio) return;
 
-    key.classList.add('playing');
-    audio.currentTime = 0;
-    audio.play();
-  }
+//     key.classList.add('playing');
+//     audio.currentTime = 0;
+//     audio.play();
+//   }
+
+  // Create audio element
+  var audioElement = document.createElement("audio");
+
+
+  // Red hover
+  $(".red").mouseenter(function() {
+    audioElement.setAttribute("src", "./sounds/snare.wav");
+    audioElement.play();
+  });
+
+
+// Red hover
+$(".theme-button").on("click", function() {
+    audioElement.play();
+    });
+    $(".pause-button").on("click", function() {
+    audioElement.pause();
+    });
+
+  // Blue hover
+  $(".theme-button").on("click", function() {
+    audioElement.play();
+  });
+  $(".pause-button").on("click", function() {
+    audioElement.pause();
+  });
+
+    // Grey hover
+    $(".theme-button").on("click", function() {
+        audioElement.play();
+      });
+      $(".pause-button").on("click", function() {
+        audioElement.pause();
+      });
+
+      // White hover
+  $(".theme-button").on("click", function() {
+    audioElement.play();
+  });
+  $(".pause-button").on("click", function() {
+    audioElement.pause();
+  });
